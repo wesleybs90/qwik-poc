6b7bb83d006c72eabaf1148ed2e4f52b5435eb29
Wesley Soares <wesleybs90@gmail.com>
2024-07-08 19:22:51 -0300

    Add initial version for using Cloudflare R2

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,5 +1,6 @@
-import type { KVNamespace, D1Database, D1Result } from '@cloudflare/workers-types';
+import type { KVNamespace, D1Database, D1Result, R2Bucket, R2Object } from '@cloudflare/workers-types';
 
+// Cloudflare KV - get cache data
 export const getCacheKV = async (platform: QwikCityPlatform, key: string): Promise<string | null> => {
   if (platform.env) {
     const KV =  platform.env[import.meta.env.VITE_KV_VARIABLE] as KVNamespace;
@@ -11,6 +12,7 @@ export const getCacheKV = async (platform: QwikCityPlatform, key: string): Promi
   return null;
 }
 
+// Cloudflare KV - put cache data
 export const putCacheKV = async (platform: QwikCityPlatform, key: string, value: string, secondsFromNow: number ): Promise<void> => {
   if (platform.env) {
     const KV =  platform.env[import.meta.env.VITE_KV_VARIABLE] as KVNamespace;
@@ -18,11 +20,12 @@ export const putCacheKV = async (platform: QwikCityPlatform, key: string, value:
   }
 }
 
+// Cloudflare D1 - get data
 export const getD1Database = async (platform: QwikCityPlatform, tableName: string): Promise<D1Result | null> => {
   if (platform.env) {
     const D1 =  platform.env[import.meta.env.VITE_D1_VARIABLE] as D1Database;
-    const ps = D1.prepare(`SELECT * from ${tableName}`);
-    const data = await ps.all();
+    const query = D1.prepare(`SELECT * from ${tableName}`);
+    const data = await query.all();
 
     return data;
   }
@@ -30,12 +33,26 @@ export const getD1Database = async (platform: QwikCityPlatform, tableName: strin
   return null;
 }
 
-export const putD1Database = async (platform: QwikCityPlatform, tableName: string): Promise<any> => {
+// Cloudflare D1 - insert data
+export const insertD1Database = async (platform: QwikCityPlatform, tableName: string): Promise<any> => {
   if (platform.env) {
     const D1 =  platform.env[import.meta.env.VITE_D1_VARIABLE] as D1Database;
-    const ps = D1.prepare(`INSERT INTO ${tableName} (id, name) VALUES (2, Name2)`);
-    const data = await ps.all();
+    const query = D1.prepare(`INSERT INTO ${tableName} (id, name) VALUES (2, Name2)`);
+    const data = await query.all();
 
     return data;
   }
 }
+
+// Cloudflare R2 - record media into R2
+export const recordMediaR2 = async (platform: QwikCityPlatform, media: ArrayBuffer): Promise<R2Object | null> => {
+  if (platform.env) {
+    const R2 =  platform.env[import.meta.env.VITE_R2_VARIABLE] as R2Bucket;
+    const mediaValue = await R2.put('PATH_TO_MEDIA_FILE', media);
+    return mediaValue;
+  }
+
+  return null;
+}
+
+// Cloudflare R2 - get media from R2
