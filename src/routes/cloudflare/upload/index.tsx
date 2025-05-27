733ff3e6fce3097a324ff2e808be103ba264173e
Wesley Soares <wesleybs90@gmail.com>
2025-05-27 13:34:15 -0300

    Adicionar nova rota para teste de upload

--- src/routes/cloudflare/upload/index.tsx
+++ src/routes/cloudflare/upload/index.tsx
@@ -2,9 +2,18 @@ import { component$, useSignal } from '@builder.io/qwik';
 import { routeAction$ } from '@builder.io/qwik-city';
 import { recordMediaR2 } from '~/services/utils';
  
-export const useUploadFile = routeAction$(async ({ file }, { platform }) => {
+export const useUploadFile = routeAction$(async ({ file, fileName, fileType }, { platform }) => {
 
-  const r2Value = await recordMediaR2(platform, 'test-file-name.jpg', file);
+  if (!file || !fileName || !fileType) {
+    return {
+      success: false,
+      error: 'File, fileName, and fileType are required.',
+    };
+  }
+
+  const fileNameWithType = `${new Date().toISOString()}-${fileName}`;
+
+  const r2Value = await recordMediaR2(platform, fileNameWithType, file);
   if (r2Value) {
     console.log('Recorded to R2 (log on server)');
   }
@@ -24,13 +33,27 @@ export default component$(() => {
         onClick$={async () => {
           
           const file = fileUploadRef.value?.files?.[0];
- 
-          if (file){
-            const formData = new FormData();
-            formData.append('file', file);
-            const { value } = await action.submit(formData);
-            console.log(value);
+          const fileName = file?.name;
+          const fileType = file?.type;
+
+          if (!file || !fileName || !fileType) {
+            console.error('File, fileName, and fileType are required.');
+            return;
           }
+          
+          const formData = new FormData();
+          formData.append('file', file);
+          formData.append('fileName', fileName);
+          formData.append('fileType', fileType);
+          const { value } = await action.submit(formData);
+          console.log(value);
+
+          if (value.success) {
+            console.log('File uploaded successfully');
+          } else {
+            console.error('File upload failed:', value.error);
+          }
+
         }}
       >
         Upload file
